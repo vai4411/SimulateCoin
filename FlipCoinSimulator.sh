262daec692a515aa790f25d909adb8a163cffd60
root <root@localhost.localdomain>
2020-03-22 22:03:45 +0000

    First Commit

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,16 +1,26 @@
 #!/bin/bash -x
 
-read -p "Enter number of times:" n
-for ((i=0; i<$n; i++))
+head=0
+tail=0
+while [ $head -lt 21 ] && [ $tail -lt 21 ]
 do
-	r=$((RANDOM % 2))
-	if [ $r -eq 1 ]
-	then
-        	head=$((head + 1))
-	else
-        	tail=$((tail + 1))
-	fi
+        r=$((RANDOM % 2))
+        if [ $r -eq 1 ]
+        then
+                head=$(($head + 1))
+        else
+                tail=$(($tail + 1))
+        fi
 done
 
-echo "Head is $head"
-echo "Tail is $tail"
+echo $head
+echo $tail
+
+if [ $head -gt $tail ]
+then
+	win=$(($head - $tail))
+	echo "Head wins by $win"
+else
+	win=$(($tail - $head))
+	echo "Tail wins by $win"
+fi
